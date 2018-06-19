ab6d475fe0a5e5efba36ea39b7511bd1b91e3bb6
RideWindX <ridewindx@163.com>
2018-06-19 19:43:56 +0800

    Add skeleton

---
+++ src/components/sections/SectionEvents.vue
@@ -0,0 +1,67 @@
+<template lang="pug">
+part#section-events.ni-section-events(title="Recent & Upcoming Events")
+  div(slot="menu"): router-link(:to="{ name: 'events' }") [All Events]
+  card-event-small(v-for="e in filteredEvents" :event="e" :key="e.id")
+</template>
+
+<script>
+import moment from "moment"
+import { orderBy, reverse } from "lodash"
+import { mapGetters } from "vuex"
+import CardEventSmall from "cards/NiCardEventSmall"
+import Part from "common/NiPart"
+export default {
+  name: "section-events",
+  components: {
+    CardEventSmall,
+    Part
+  },
+  computed: {
+    filteredEvents() {
+      let events = this.events.filter(e => e.dates.start !== undefined)
+      events = orderBy(
+        events,
+        [
+          function(e) {
+            return moment(e.dates.start)
+          }
+        ],
+        "desc"
+      )
+      return reverse(events.slice(0, 4))
+    },
+    ...mapGetters(["events"])
+  }
+}
+</script>
+
+<style lang="stylus">
+@require '~variables'
+
+.ni-section-events
+  border-top 1px solid bc
+
+.ni-section-events .ni-event-sm__container:nth-of-type(1n+3)
+  display none
+
+@media screen and (min-width:768px)
+  .ni-section-events .ni-event-sm__container:nth-of-type(3)
+    display block
+
+  .ni-section-events
+    .ni-part-main
+      display flex
+      flex-flow row wrap
+
+      .ni-event-sm__container
+        flex 0 0 33.333%
+
+@media screen and (min-width: 1024px)
+  .ni-section-events .ni-event-sm__container:nth-of-type(4)
+    display block
+
+  .ni-section-events
+    .ni-part-main
+      .ni-event-sm__container
+        flex 0 0 25%
+</style>
