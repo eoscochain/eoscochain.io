84d632e769e4f0a5ccfcbe8f6b515d2fa4941f62
RideWindX <ridewindx@163.com>
2018-07-01 11:44:41 +0800

    Delete plan, academy, fundraiser

--- src/components/pages/PageResources.vue
+++ src/components/pages/PageResources.vue
@@ -4,9 +4,9 @@
     router-link(:to="{name: 'resources'}") {{ $t('Overview') }}
     router-link(:to="{name: 'whitepaper'}") {{ $t('Whitepaper') }}
     router-link(:to="{name: 'faq'}") {{ $t('FAQ') }}
-    router-link(:to="{name: 'plan'}") {{ $t('Plan') }}
-    router-link(:to="{name: 'academy'}") {{ $t('Academy') }}
-    router-link(:to="{name: 'delegators'}") {{ $t('Delegators') }}
+    // router-link(:to="{name: 'plan'}") {{ $t('Plan') }}
+    // router-link(:to="{name: 'academy'}") {{ $t('Academy') }}
+    // router-link(:to="{name: 'delegators'}") {{ $t('Delegators') }}
   router-view
 </template>
 
